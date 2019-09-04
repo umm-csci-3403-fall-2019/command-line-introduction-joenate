28294ec799e116d48884f65ed9633fe3ea3ceb9d
deglm006 <deglm006@morris.umn.edu>
2019-09-04 16:12:23 -0500

    Cleaned up recompress code and added comments

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-tar -zxf $1 --directory $2
+tar -zxf "$1" --directory "$2"
 
-dirString=$1
+dirString="$1"
 newDir=${dirString/.tgz}
 
-cd $newDir
+cd "$newDir"
 
-#SAFETY FIRST!
+#SAFETY FIRST! Use this variable so we don't get the target grep pattern in our script
 deleteStr="DELETE"
+
+# remove the list of files (found by grep) that contain the target pattern (DELETE + ME!)
 rm $(grep "${deleteStr} ME!" $(ls) -l)
 
-cleanedStr="../cleaned_${newDir}.tgz"
-tar -czf $cleanedStr ../$newDir 
+cd ..
+
+cleanedStr="cleaned_${newDir}.tgz"
+tar -czf $cleanedStr $newDir 
 
 echo "done"
